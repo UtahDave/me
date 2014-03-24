b96babcae58b98b83e915704b6b3c9a601c83434
David Boucha <boucha@gmail.com>
2014-03-24 07:48:18 -0600

    improve environment verification
    Make sure ~/.me exists
    Make sure ~/.me/tasks.sls exists

--- me/utils/__init__.py
+++ me/utils/__init__.py
@@ -6,11 +6,19 @@ Common utilities
 
 #import python libs
 import os
+from os.path import expanduser
 
 def verify_env():
     '''
     Make sure that all required files exist
     '''
     print('yes')
-    if not os.path.exists('~/.me/'):
-        os.mkdir('~/.me/')
+    home = expanduser('~')
+    print(home)
+    taskfilepath = os.path.join(home, '.me')
+    print(taskfilepath)
+    if not os.path.exists(taskfilepath):
+        os.makedirs(taskfilepath)
+    if not os.path.isfile(os.path.join(taskfilepath, 'tasks.sls')):
+        with open(os.path.join(taskfilepath, 'tasks.sls'), 'w') as taskfile:
+            taskfile.write('taskfile')
